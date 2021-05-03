2e37babb6322277f066aeb0e0b0bfdacfe17beab
Kevin Liang <47095084+unko-chan@users.noreply.github.com>
2021-05-02 20:31:49 -0700

    add poster and year display; remove duplicate listings

--- src/components/Search/Results.js
+++ src/components/Search/Results.js
@@ -1,12 +1,23 @@
 import React, { useState, useEffect } from "react";
+let _ = require('lodash')
 
 function Results(props) {
   const [searchResults, setSearchResults] = useState([]);
   useEffect(() => {
     props.results ? setSearchResults(props.results) : setSearchResults([]);
   }, [props.results]);
-  return searchResults.map((movie) => {
-    return <div>{movie.Title}</div>;
+
+  return _.uniqBy(searchResults, 'imdbID').map((movie) => {
+    return (
+      <article className="movie" key={movie.imdbID}>
+        <img className="movie__poster" src={movie.Poster} />
+        <div className="movie__info">
+          <div className="move__title">{movie.Title}</div>
+          <div className="movie__year">{movie.Year}</div>
+          <a href={`https://www.imdb.com/title/${movie.imdbID}`}>IMDb</a>
+        </div>
+      </article>
+    );
   });
 }
 
