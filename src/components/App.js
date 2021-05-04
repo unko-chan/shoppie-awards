86b293f054f83872e6ead3ffde0b9d1a81978e68
Kevin Liang <47095084+unko-chan@users.noreply.github.com>
2021-05-03 22:14:25 -0700

    add nomination state

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,20 @@
-import '../styles/App.scss';
-import Search from "./Search/Search.js"
+import React, { useState } from "react";
+import "../styles/App.scss";
+import Search from "./Search/Search.js";
+import NominateList from "./Nominate/NominateList.js";
 
 
-function App() {
+
+function App(props) {
+
+  const setMovie = (movie) => setNomination([...nomination, movie])
+  const [nomination, setNomination] = useState([])
+  console.log(nomination)
+
   return (
     <div className="app">
-      <Search/>
+      <Search setMovie={setMovie}/>
+      <NominateList nominations={nomination}/>
     </div>
   );
 }
