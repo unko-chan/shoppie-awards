6c73bb2e43864ef79f1449505b84aa3b4f0ce6c6
Kevin Liang <47095084+unko-chan@users.noreply.github.com>
2021-05-05 01:08:28 -0700

    Refactor to use styled-components

--- src/components/MovieListItem.js
+++ src/components/MovieListItem.js
@@ -1,20 +1,40 @@
 import React from "react";
 import Button from "./Button";
+import styled from "styled-components";
 
+const MovieContainer = styled.article`
+  display: flex;
+  align-items: center;
+  border: solid rgba(0, 0, 0, 0.233) 1px;
+`;
+
+const Poster = styled.img`
+  width: 48px;
+`;
+
+const Movie = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex: 1;
+`;
 
 function MovieListItem(props) {
   return (
-    <article className="movie__container" key={props.imdbID}>
-      <img className="movie__poster" src={props.Poster} />
-      <div className="movie__info">
-        <div>{props.Title}</div>
-        <div>{props.Year}</div>
-        <a href={`https://www.imdb.com/title/${props.imdbID}`}>IMDb</a>
-      </div>
-      <div className="button">
-        <Button color={props.color} onClick={props.onClick}>{props.button}</Button>
-      </div>
-    </article>
+    <MovieContainer key={props.imdbID}>
+      <Poster src={props.Poster} />
+      <Movie>
+        <div>
+          <div>{props.Title}</div>
+          <div>{props.Year}</div>
+          <a href={`https://www.imdb.com/title/${props.imdbID}`}>IMDb</a>
+        </div>
+        <div>
+          <Button color={props.color} onClick={props.onClick}>
+            {props.button}
+          </Button>
+        </div>
+      </Movie>
+    </MovieContainer>
   );
 }
 
