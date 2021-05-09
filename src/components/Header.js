6152bf3bcc9a760edf42a52c325051c00dd21e9a
Kevin Liang <47095084+unko-chan@users.noreply.github.com>
2021-05-09 14:33:31 -0700

    Create banner image

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from "styled-components"
+import banner from "../img/banner.png"
+
+const StyledHeader = styled.img`
+    width: 450px;
+    align-self: center;
+    margin: 2rem;
+`
+
+function Header(props) {
+    return (
+        <StyledHeader src={banner}/>
+    )
+}
+
+export default Header
